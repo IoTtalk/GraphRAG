49f7a72ae3a51f1d4aade8a2a93e2bda2644d005
zyj@MS <edenzyj88@gmail.com>
2024-08-01 17:01:21 +0800

    complete grab.py

---
+++ grab.py
@@ -0,0 +1,11 @@
+import requests
+import os
+
+file = "input/book.txt"
+
+if not os.path.exists(file) :
+    url = "https://www.gutenberg.org/cache/epub/24022/pg24022.txt"
+    response = requests.get(url, stream=True)
+    data = response.text
+    with open(file, "w", encoding="utf-8") as f:
+        f.write(data)
